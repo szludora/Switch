8de2270a92280752849d1dc513cd1102c89335c9
szludora <szlucska.dora@gmail.com>
2023-11-15 03:11:40 +0100

    add header, body, city.png

--- switch.js
+++ switch.js
@@ -1,11 +1,15 @@
 $(function () {
+  let body = $("body");
+  let header = $("header")
   let swC = $(".switchContainer");
   let swB = $(".switchButton");
-  let rocket = $(".rocket")
-  let astronaut = $(".astronaut")
+  let rocket = $(".rocket");
+  let astronaut = $(".astronaut");
 
   swB.on("click", () => {
     setTimeout(() => {
+      body.toggleClass("bodyOff");
+      header.toggleClass("headerDark")
       swB.toggleClass("off");
       rocket.toggleClass("Rup");
       astronaut.toggleClass("Aup");
